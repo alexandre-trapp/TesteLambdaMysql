4be52d3e487f0f7eca01775e795e66f4d48255aa
Alexandre Cordeiro Trapp <alexandre.trapp@warren.com.br>
2023-02-17 00:34:35 -0300

    add database mysql

--- Function.cs
+++ Function.cs
@@ -1,5 +1,9 @@
 using Amazon.Lambda.Core;
 using Amazon.Lambda.SQSEvents;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Data.Common;
+using System.Diagnostics.Metrics;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -25,9 +29,36 @@ public class Function
 
             Console.WriteLine($"Record Body:");
             Console.WriteLine(record.Body);
+
+            SetInDatabase(record.Body);
         }
 
         Console.WriteLine("Processing complete.");
         return $"Processed {sqsEvent.Records.Count} records.";
+    }
+
+    private void SetInDatabase(string body)
+    {
+        string configDB = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+
+        using var _connection = new MySqlConnection(configDB);
+
+        if (_connection.State == ConnectionState.Closed)
+            _connection.Open();
+
+        using var cmd = new MySqlCommand("INSERT INTO TESTE(objeto) VALUES(?objeto) ", _connection);
+        cmd.Parameters.Add("?objeto", MySqlDbType.VarChar).Value = body;
+
+        cmd.ExecuteNonQuery();
+
+        using var cmd2 = new MySqlCommand("SELECT * FROM TESTE", _connection);
+        using var reader = cmd2.ExecuteReader();
+
+        Console.WriteLine("Log: reader.FieldCount: " + reader.FieldCount);
+
+        while (reader.Read())
+        {
+            Console.WriteLine($"Objeto = {reader["objeto"]}");
+        }
     }
 }
